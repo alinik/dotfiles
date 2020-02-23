b03864da2fcb70f75859c8228c2c56cbebda88de
علی نیک نشان <nikneshan@baregheh.com>
2020-02-23 14:28:40 +0330

    add sudoer file

--- bin/daily_sync.sh
+++ bin/daily_sync.sh
@@ -7,7 +7,7 @@ zsync http://cdimage.ubuntu.com/bionic/daily-live/current/bionic-desktop-amd64.i
 zsync http://cdimage.ubuntu.com/ubuntu-server/bionic/daily/current/bionic-server-amd64.iso.zsync
 
 #sync external git
-for i in /data/git-sync/*;do pushd $i; git fetch --quiet origin ;git push --quiet work; popd;done
+for i in /data/git-sync/*;do pushd $i; git fetch --quiet origin ;git push --mirror --quiet work; popd;done
 
 #Update all server's packages
 cd /home/ali/ansible && ansible-playbook update_packages.yaml
