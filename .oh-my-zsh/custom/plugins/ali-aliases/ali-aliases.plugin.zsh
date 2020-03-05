874157bfbf9f1fbd2b3429b82a64c93c0be825ee
علی نیک نشان <nikneshan@baregheh.com>
2020-03-05 12:43:15 +0330

    add sudo aliases for nonroot users

--- .oh-my-zsh/custom/plugins/ali-aliases/ali-aliases.plugin.zsh
+++ .oh-my-zsh/custom/plugins/ali-aliases/ali-aliases.plugin.zsh
@@ -4,7 +4,12 @@ alias catc='pygmentize -g'
 alias dm='du -sxm *|sort -nr|head'
 alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias tailf='tail -f'
-
+if [ `id -u` -ne 0 ]; then
+    for cmd in apt iptables ip ss smem dpkg apt-get snap systemctl chown ntpdate ;
+        do
+           alias $cmd="sudo $cmd"
+        done
+fi
 function nocc(){
 	pygmentize -g $1|grep -Ev '^\s*.{0,5}#|^$'
 }
