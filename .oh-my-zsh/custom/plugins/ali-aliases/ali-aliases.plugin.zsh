d866a87fb1a5254d3b1ef8126f3cfe5fec67e924
علی نیک نشان <nikneshan@baregheh.com>
2022-05-28 14:13:35 +0430

    add csf

--- .oh-my-zsh/custom/plugins/ali-aliases/ali-aliases.plugin.zsh
+++ .oh-my-zsh/custom/plugins/ali-aliases/ali-aliases.plugin.zsh
@@ -5,6 +5,7 @@ alias dm='du -sxm *|sort -nr|head'
 alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias cmdb='git --git-dir=/root/.cmdb --work-tree=/' # mkdir /root/.cmdb && cd /root/.cmdb && git init --bare . 
 alias tailf='tail -f'
+alias csf='sudo csf'
 if [ `id -u` -ne 0 ]; then
     for cmd in apt iptables ip ss smem dpkg apt-get snap systemctl chown ntpdate ;
         do
