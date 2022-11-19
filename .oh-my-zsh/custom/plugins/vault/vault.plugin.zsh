2fa1b8e289fac3171c527ccd21c06b079b9152ac
Ali Nikneshan <ali@mybitmax.com>
2022-11-19 09:56:10 +0330

    add vault sealcheck

--- .oh-my-zsh/custom/plugins/vault/vault.plugin.zsh
+++ .oh-my-zsh/custom/plugins/vault/vault.plugin.zsh
@@ -1,4 +1,4 @@
 [ -e /usr/bin/vault ] || exit 0
 autoload -U +X bashcompinit && bashcompinit
 complete -o nospace -C /usr/bin/vault vault
-
+#vault status -format=yaml |grep -q "sealed: true" && echo Vault $VAULT_ADDR Sealed!
