49b2dc596ff1e870e7c1a2ae5df239ae24ce7e27
Ali Nikneshan <ali@mybitmax.com>
2023-09-10 16:51:55 +0330

    update db ip:

--- bin/firewall-under-attak.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/16ba1ded01e49db0a0e6c9356647ba49/settings/security_level" -H "Authorization: Bearer qhTngdSH2LDjKNK2lAKFBWske63F945DImnJCmjl" -H "Content-Type:application/json" -d '{"value":"under_attack"}'|jq .success
